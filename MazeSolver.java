774ed6726e3c81c204847ac4fb66e39e52b256be
Nikolaka17 <92060768+Nikolaka17@users.noreply.github.com>
2023-03-06 09:19:44 -0600

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,3 +1,9 @@
+//Nikolas Leslie
+//Program that uses recursion to solve a maze
+
+/**
+  *Class that loads a maze to solve
+*/
 public class MazeSolver {
     
     public static void main(String[] args){
